ea363a9aa26b0da5eaa385810b9645c94be15ac6
Louis Walker <phetablack@gmail.com>
2016-11-12 02:58:09 +0000

    SubtitleService refactor

--- src/app/subtitle.service.ts
+++ src/app/subtitle.service.ts
@@ -2,19 +2,11 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SubtitleService {
-  private subs: Array<String> = [];
-
-  constructor() {
-    this.generateSubs();
-  }
-
-  generateSubs() {
-    this.subs = [
-      "Welcome!",
-      "Discover your soul.",
-      "Yummy Mtn. Dew"
-    ];
-  }
+  private subs: Array<String> = [
+    "Welcome!",
+    "Discover your soul.",
+    "Yummy Mtn. Dew"
+  ];
 
   getRandom() {
     return this.subs[ Math.floor( Math.random() * this.subs.length) ];
